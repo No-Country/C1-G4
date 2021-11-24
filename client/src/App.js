e5472e6a3bdf1850666fb6877ee9d49bb41a4d2b
Sebastian Campos <bastiampos.uni@gmail.com>
2021-11-24 16:40:37 -0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import './App.css'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+
+import userActions from './redux/actions/userActions'
+
+const App = () => {
+   const user = useSelector(state => state.auth.user)
+   const dispatch = useDispatch()
+
+   useEffect(() => {
+      if (localStorage.getItem('token')) {
+         dispatch(userActions.loginUserLS(localStorage.getItem('token')))
+      }
+   }, [])
+
+   return (
+      <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='signin' element={<Signin />} />
+            <Route path='signup' element={<Signup />} />
+         </Routes>
+      </BrowserRouter>
+   )
 }
 
-export default App;
+export default App
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import { applyMiddleware, createStore } from 'redux'
+import { Provider } from 'react-redux'
+import mainReducer from './redux/reducers/mainReducer'
+import thunk from 'redux-thunk'
+
+const globalStore = createStore(mainReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+   <React.StrictMode>
+      <Provider store={globalStore}>
+         <App />
+      </Provider>
+   </React.StrictMode>,
+   document.getElementById('root')
 );
