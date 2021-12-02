421d0e196761d5413bd4be9d34d010fa7365c84a
Dafk <nadaeso124@gmail.com>
2021-12-02 17:48:35 -0600

    Reservation configs iniciales

---
+++ api/src/controllers/ReservationControllers.js
@@ -0,0 +1,108 @@
+import models from "../models/models";
+
+export default {
+  // creando reservación  
+  createReservation: async (req, res, next) => {
+    try {
+      const reservation = await models.Reservation.create(req.body);
+
+      if (reservation) {
+        res.status(200).json({ reservation });
+      }
+      
+      res.json({ msg:'Deves llenar todos los datos requires para acer una reservación' })
+    } catch (e) {
+      
+      res.status(500).send({
+        message: "Eror al intentar agregar reservación",
+      });
+      
+      next(e);
+    }
+  },
+  
+  // obtener todas la reservaciones creadas
+  getlistReservation: async (req, res, next) => {
+    try {
+      const reservation = await models.Reservation.find();
+      res.status(200).json(reservation);
+    } catch (e) {
+      
+      res.status(500).send({
+        message: "Eror al intentar listar las reservaciónes",
+      });
+      
+      next(e);
+    }
+  },
+  
+  updateReservation: async (req, res, next) => {
+    try {
+      const reservation = await models.Reservation.findByIdAndUpdate(
+        { _id: req.body._id },
+        { name: req.body.name }
+      );
+      res.status(200).json(reservation);
+    } catch (e) {
+      
+      res.status(500).send({
+        message: "Error al intentar actualizar reservación",
+      });
+      
+      next(e);
+    }
+  },
+  
+  removeReservation: async (req, res, next) => {
+    try {
+      // obteniendo reservación  
+      const reservation = await models.Reservation.findByIdAndDelete({
+        _id: req.body._id
+      });
+     
+      res.status(200).json(reservation);
+    } catch (e) {
+      
+      res.status(500).send({
+        message: "Error al intentar eliminar reservación",
+      });
+      
+      next(e);
+    }
+  },
+  
+  activateReservation: async (req, res, next) => {
+    try {
+      const reg = await models.Reservation.findByIdAndUpdate(
+        { _id: req.body._id },
+        { state: 1 }
+      );
+      
+      res.status(200).json(reg);
+    } catch (e) {
+      
+      res.status(500).send({
+        message: "Error al intentar activar reservación",
+      });
+      next(e);
+    }
+  },
+  
+  desactivateReservation: async (req, res, next) => {
+    try {
+      const reservation = await models.Reservation.findByIdAndUpdate(
+        { _id: req.body._id },
+        { state: 0 }
+      );
+      
+      res.status(200).json(reservation);
+    } catch (e) {
+      
+      res.status(500).send({
+        message: "Eror al intentar desactivar reservación",
+      });
+      next(e);
+    }
+
+  }
+};
--- api/src/routes/routes.js
+++ api/src/routes/routes.js
@@ -1,11 +1,15 @@
 import routerx from "express-promise-router";
 import specialityRouter from "./speciality";
 import medicRouter from "./medic";
+import reservationRouter from "./reservation";
 
 const router = routerx();
 
 router.use("/speciality", specialityRouter);
 
 router.use("/medic", medicRouter);
+
+// rutas de reservación
+router.use("/reservation", reservationRouter);
 
 export default router;
