96f6c674acf69c2751854e344f160666f526758e
Rene Ramirez <rdejramirez@gmail.com>
2021-12-02 17:31:09 -0500

    middleware auth implementado user controller pequeños cambios y probado en postman incluyendo login y protegiendo las rutas con json web token

--- api/src/models/user.js
+++ api/src/models/user.js
@@ -9,12 +9,10 @@ const userSchema = new Schema({
     name: {
         type: String,
         required: true,
-        unique: true,
     },
     lastname: {
         type: String,
         required: true,
-        unique: true,
     },
     email: {
         type: String,
