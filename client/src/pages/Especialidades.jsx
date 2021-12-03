1baffec9f34575cc2570ecc9c437f2642ac53f43
Rene Ramirez <rdejramirez@gmail.com>
2021-12-03 16:48:26 -0500

    maquetacion de Especialidades.jsx

--- client/src/pages/Especialidades.jsx
+++ client/src/pages/Especialidades.jsx
@@ -1,7 +1,56 @@
+import styles from "./../styles/especialidades.module.css";
+
 const Especialidades = () => {
   return (
-    <div>
-      <h3> Especialidades </h3>
+    <div className={styles.contenedorEspecialidades}>
+      <div className={styles.contenedorTitulo}>
+        <h3 className={styles.titulo}> Especialidades </h3>
+      </div>
+      <div className={styles.contenedorLista}>
+        <div className={styles.card}>
+          <div className={styles.data}>
+            <p className={styles.name}> Otorrinolaringologia </p>
+          </div>
+          <div className={styles.opc}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className={"bi bi-caret-down-square-fill " + styles.icono}
+              viewBox="0 0 16 16"
+            >
+              <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4 4a.5.5 0 0 0-.374.832l4 4.5a.5.5 0 0 0 .748 0l4-4.5A.5.5 0 0 0 12 6H4z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className={
+                "bi bi-trash-fill " + styles.icono + " " + styles.trash
+              }
+              viewBox="0 0 16 16"
+            >
+              <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
+            </svg>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              className={"bi bi-pencil-square " + styles.icono + ' ' + styles.editar}
+              viewBox="0 0 16 16"
+            >
+              <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+              <path
+                fill-rule="evenodd"
+                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"
+              />
+            </svg>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
