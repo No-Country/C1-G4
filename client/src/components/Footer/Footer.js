d05dbddf4e254139a28faad26a95e69f55d3f928
CanCerber0 <geronimo.bknuttzen@gmail.com>
2021-12-05 18:50:31 -0300

    cambios Footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -2,57 +2,39 @@ import React, { Fragment } from "react";
 import styles from "../../styles/Footer.module.css";
 import logo from "../../images/medic.svg";
 
-function Footer() {
+function Footer() { 
   return (
     <Fragment>
-      <div>
-        <footer className="divfooter" style={styles}>
-          <div id="linksfooter" className={styles.divs}>
-            <h4>Links</h4>
-            <ul>
-              <li>
-                <a href="https://www.google.com.ar">Nosotros</a>
-              </li>
-              <li>
-                <a href="# ">Preguntas frecuentes</a>
-              </li>
-              <li>
-                <a href="# ">Terminos legales</a>
-              </li>
-              <li>
-                <a href="# ">Ayuda</a>
-              </li>
-            </ul>
-          </div>
-
-          <div id="legalfooter" className={styles.divs}>
-            <h4>Legal</h4>
-            <ul>
-              <li>
-                <a href="# ">Terminos legales</a>
-              </li>
-              <li>
-                <a href="# ">Politicas de privacidad</a>
-              </li>
-              <li>
-                <a href="# ">Solicitud de baja</a>
-              </li>
-            </ul>
-          </div>
-
-          <hr />
-        </footer>
-        <div className="footerBajo">
-          <div className={styles.divContacto}>
-            <img className={styles.logo} src={logo} alt="" />
+    <footer>
+      <div className={styles.upFooter}>
+        <div>
+          <h4 className={styles.h4}>Links</h4>
+            <a href="#">Nosotros<br /></a>
+            <a href="#">Preguntas frecuentes<br /></a>
+            <a href="#">Terminos legales<br /></a>
+            <a href="#">Ayuda<br /></a>
+        </div>
+        <div>
+          <h4 className={styles.h4}>Legal</h4>
+            <a href="#">Politicas de privacidad<br /></a>
+            <a href="#">Terminos legales <br /></a>
+            <a href="#">Solicitud de baja<br /></a>
+        </div>
+      </div>
+      </footer>
+      <div className={styles.lowFooter}>
+        <div className={styles.elements}>
             <p>
               Avenida Calle 1500 <br />
               Ciudad de BA, CP 1500
             </p>
-            <p>Contactanos: 111 222 6789</p>
-          </div>
+            <img className={styles.logo} src={logo} alt="" />
+            <p>
+              Contactanos: <br /> 
+              111 222 6789
+            </p>
         </div>
-      </div>
+      </div>  
     </Fragment>
   );
 }
