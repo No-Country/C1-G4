d25e76d085df967e86269f6ba7a5e3053bfe4a06
CanCerber0 <geronimo.bknuttzen@gmail.com>
2021-12-08 17:37:29 -0300

    cambios

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,7 +3,7 @@ import styles from "../styles/home.module.css";
 const Home = () => {
   return (
     <div className={styles.divs}>
-      <h1>BIENVENIDOS A MEDIC CENTER</h1>
+      <h1>BIENVENIDO A MEDIC CENTER</h1>
     </div>
   );
 };
