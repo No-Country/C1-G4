b94c1ccd6a9e3e22e5a9dce5ee2255987a0a23f3
CanCerber0 <geronimo.bknuttzen@gmail.com>
2021-12-08 15:46:51 -0300

    cambios CardModule

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,32 +1,33 @@
 import React, { Fragment } from "react";
 import styles from "../../styles/Card.module.css";
 import logo from "../../images/medic.svg";
+import { Link } from "react-router-dom";
 
 function Card() {
   return (
     <Fragment>
       <div className={styles.container}>
-        <a href="#" className={styles.btn}>
+        <Link to="#" className={styles.btn}>
           <div className={styles.boton}>
             <h4>Turnos Disponibles</h4>
             <img src={logo} alt="" />
             <p>Ingresa aqui</p>
           </div>
-        </a>
-        <a href="#" className={styles.btn}>
+        </Link>
+        <Link to="#" className={styles.btn}>
           <div className={styles.boton}>
             <h4>Historial Clinico</h4>
             <img src={logo} alt="" />
             <p>Ingresa aqui</p>
           </div>
-        </a>
-        <a href="#" className={styles.btn}>
+        </Link>
+        <Link to="/especialidades" className={styles.btn}>
           <div className={styles.boton}>
             <h4>Cartilla Médicos</h4>
             <img src={logo} alt="" />
             <p>Ingresa aqui</p>
           </div>
-        </a>
+        </Link>
       </div>
     </Fragment>
   );
