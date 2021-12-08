3cf766a52da358c5e06697a3aa0539a5dfc0764a
CanCerber0 <geronimo.bknuttzen@gmail.com>
2021-12-08 15:11:10 -0300

    cambios header. carrusel

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,12 +1,22 @@
 import React, { Fragment } from "react";
 import estilo from "../../styles/Header.module.css";
 import logo from "../../images/medic.svg";
-import Nav from "../Nav/Nav.js";
 
 function Header() {
   return (
     <Fragment>
-      <Nav />
+      <div className={estilo.container}>
+        <img className={estilo.logo} alt="" src={logo} />
+        <div>
+          <nav>
+            <ul className={estilo.ulestilo}>
+              <li>Home</li>
+              <li>Iniciar sesion</li>
+              <li>Registrarse</li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </Fragment>
   );
 }
